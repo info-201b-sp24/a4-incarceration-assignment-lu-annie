816cfdbff45fe2f26e4936c9143cd1d476a76656
Annie Lu <annielu@Yomommas-macAIRoni-and-cheese.local>
2024-05-19 23:50:08 -0700

    summary.R setup

---
+++ summary.R
@@ -0,0 +1,14 @@
+library(dplyr)
+summary_info <- list()
+
+# highest value race ratio most recent year
+
+# lowest value race and ratio most recent year
+
+# highest value race ratio and county
+
+
+
+# highest value race ratio and year
+
+# highest value race ratio and rurality
